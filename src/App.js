efe2256f6d377515039a6402ebd7686517822b05
JunioBatista <juniorjob21@gmail.com>
2022-02-02 21:50:56 -0300

    adicionei o footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Routes from "./routes/routes";
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from "./styles/theme/theme";
+import Footer from "./components/Footer/index"
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Routes/>
+      <Footer />
     </ChakraProvider>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Image } from "@chakra-ui/react";
+import footerImage from '../../assets/footer.jpg'
+
+const Footer = () => {
+    return  (
+        <Image 
+        alt="Gramado e logo labedex"
+        src={footerImage} 
+        w={"100%"} 
+        h={"100%"}
+
+        objectFit='cover'/>
+    )
+
+}
+
+export default Footer
