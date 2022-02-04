154f90bb7139a07397ebe8d47d4eea6cee7ea88b
JunioBatista <juniorjob21@gmail.com>
2022-02-04 01:41:34 -0300

    adicionei a ordenação por nome

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,19 @@
-import { Box, Grid } from "@chakra-ui/react";
+import { Box, Grid, Select } from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import PokemonCard from "../../components/PokemonCard";
+import { SelectContainer } from './styled'
+
 
 function HomePage(props) {
 
-//já está sendo recebida as props de pesquisa do header.
-
   const [pokemons, setPokemons] = useState([]);
+  const [sortParameter, setsortParameter] = useState ("default")
+
+  const handleSortParameter = ({target}) => {
+    setsortParameter(target.value)
+    console.log(target.value)
+  }
 
   useEffect(() => {
     axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
@@ -15,19 +21,39 @@ function HomePage(props) {
     });
   }, []);
 
-  console.log(pokemons[0].name)
-  console.log(props.searchPokemon)
-
+console.log(pokemons)
   return (
-    <Box display={"flex"} justifyContent={"center"}>
-    <Grid p={"2em"} templateColumns='repeat(4, 1fr)' gap={4} >
-      {pokemons?.filter(pokemon => {
-        return  pokemon.name.includes(props.searchPokemon)
-      }).map((pokemon) => {
-        return <PokemonCard key={pokemon.name} pokemons={pokemon} />;
-      })}
-    </Grid>
-    </Box>
+    <div>
+      <SelectContainer display={"flex"}>
+        <Select 
+        value={sortParameter}
+        onChange={handleSortParameter}
+        fontFamily={"Flexo-Demi"}
+        >
+          <option value={"default"} disabled >Ordenar por</option>
+          <option value={"a-z"}>A-Z</option>
+          <option value={"tipo"}>Tipo</option>
+        </Select>
+      </SelectContainer>
+      <Box display={"flex"} justifyContent={"center"} >
+        <Grid p={"2em"} templateColumns='repeat(4, 1fr)' gap={4} >
+
+          {pokemons?.filter(pokemon => {
+            return pokemon.name.includes(props.searchPokemon)
+          })
+          .sort((currentPokemon, nextPokemon)=>{
+            if (sortParameter === "a-z"){
+              return currentPokemon.name.localeCompare(nextPokemon.name)
+            }
+            // if (sortParameter === ""){}
+          })
+          .map((pokemon) => {
+            return <PokemonCard key={pokemon.name} pokemons={pokemon} />;
+          })
+          
+          }
+        </Grid>
+      </Box></div>
   );
 }
 
---
+++ src/pages/HomePage/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const SelectContainer = styled.div`
+
+width:150px;
+justify-content: flex-end;
+margin: .5em 0 0 75vw;
+flex-direction: column;
+`
